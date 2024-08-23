9ec5febcf38f05ca0c6dd6e8b7c53b71bee0aec1
AhmedHelal12 <ahmedhelalragab@gmail.com>
2024-08-23 18:30:02 +0300

    init the models

--- django_pm/settings.py
+++ django_pm/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'django_pm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projects_management',
+        'USER':'postgres',
+        'PASSWORD':'ahmedmido12',
+        'HOST': 'localhost',
+        'PORT' : ''
     }
 }
 
