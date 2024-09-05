9719f1c9927dcbd099c36b559d95b020f8677b91
AhmedHelal12 <ahmedhelalragab@gmail.com>
2024-09-06 00:50:09 +0300

    add arabic translations into the website

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
-
+from django.utils.translation import gettext as _
 attrs = {'class':'form-control'}
 
 
@@ -11,12 +11,12 @@ class UserLoginForm(AuthenticationForm):
         super(UserLoginForm, self).__init__(*args, **kwargs)
     
     username = forms.CharField(
-        label="Username",
+        label=_("Username"),
         widget = forms.TextInput(attrs=attrs)
     )
 
     password = forms.CharField(
-        label="Password",
+        label=_("Password"),
         widget = forms.PasswordInput(attrs=attrs)
     )
 
@@ -24,32 +24,32 @@ class UserLoginForm(AuthenticationForm):
 class UserRegisterForm(UserCreationForm):
 
     first_name = forms.CharField(
-        label="First Name",
+        label=_("First Name"),
         widget=forms.TextInput(attrs=attrs)
     )
 
     last_name = forms.CharField(
-        label="Last Name",
+        label=_("Last Name"),
         widget=forms.TextInput(attrs=attrs)
     )
 
     email = forms.CharField(
-        label="Email",
+        label=_("Email"),
         widget=forms.EmailInput(attrs=attrs)
     )
 
     username = forms.CharField(
-        label="Username",
+        label=_("Username"),
         widget = forms.TextInput(attrs=attrs)
     )
 
     password1 = forms.CharField(
-        label="Password",
+        label=_("Password"),
         widget = forms.PasswordInput(attrs=attrs),
         strip = False
     )
     password2 = forms.CharField(
-        label="Confirm Password",
+        label=_("Confirm Password"),
         widget = forms.PasswordInput(attrs=attrs),
         strip = False
 
--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,7 @@
 
 from django import forms
 from projects.models import Project
-
+from django.utils.translation import gettext as _
 attrs={'class':'form-control','style':'margin: 10px;'}
 
 class ProjectCreateForm(forms.ModelForm):
@@ -9,10 +9,17 @@ class ProjectCreateForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title','description','category']
+       
         widgets = {
             "category" : forms.Select(attrs=attrs),
             "title": forms.TextInput(attrs=attrs),
             "description": forms.Textarea(attrs=attrs),
+        }
+
+        labels = {
+            "category" : _("Category"),
+            "title" : _("title"),
+            "description":_("description")
         }
 
         
